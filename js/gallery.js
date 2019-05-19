f8cfa76e9ab39e1172d68723c91c4dd9e558a605
petitagneaunoir <petitagneaunoir@gmail.com>
2019-05-19 16:33:56 -0400

    Finish JS gallery

---
+++ js/gallery.js
@@ -0,0 +1,22 @@
+function activateGallery() {
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+      thumbnail.addEventListener("click", function() {
+          let newImageSrc = thumbnail.dataset.largeVersion;
+          mainImage.setAttribute("src", newImageSrc);
+          mainImage.setAttribute("alt", thumbnail.alt);
+
+          document.querySelector(".current").classList.remove("current");
+          thumbnail.parentNode.classList.add("current");
+
+          let galleryInfo = document.querySelector("#gallery-info");
+          let title = galleryInfo.querySelector(".title");
+          let description = galleryInfo.querySelector(".description");
+
+          title.innerHTML = thumbnail.dataset.title;
+          description.innerHTML = thumbnail.dataset.description;
+      });
+  });
+}
